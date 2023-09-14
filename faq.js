6d236bff7954a255b2c145a48bc058ee9d6def87
kuror0lucifer <kovalishin.2014@mail.ru>
2023-09-14 19:25:20 +0200

    faq

---
+++ faq.js
@@ -0,0 +1,9 @@
+let contexts = document.querySelectorAll(".faq-div-p-context");
+let faq = document.querySelectorAll(".faq-div-block");
+
+faq.forEach((faqItem, index) => {
+  faqItem.addEventListener("click", function () {
+    contexts[index].classList.toggle("hidden");
+
+  });
+});
