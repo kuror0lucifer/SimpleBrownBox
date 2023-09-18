6078e6f9dff0bc1c71b8ec6a040b0ad8ad31f7cb
kuror0lucifer <kovalishin.2014@mail.ru>
2023-09-18 19:02:40 +0200

    reviews page

--- scripts/faq.js
+++ scripts/faq.js
@@ -4,6 +4,5 @@ let faq = document.querySelectorAll(".faq-div-block");
 faq.forEach((faqItem, index) => {
   faqItem.addEventListener("click", function () {
     contexts[index].classList.toggle("hidden");
-
   });
 });
